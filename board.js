5dbc13db07430a9e9324bb23a0c17ba0cefa79c1
sunxiangxin <sunxiangxin>
2019-06-21 09:28:44 +0800

    add image

--- board.js
+++ board.js
@@ -4,27 +4,28 @@ class DrawingBoard{
         this.dom = null;
         this.color = 'black';
         this.penWidth = 6;
+        this.goods = '';
     }
 
-    painting(ele){
-        ele.style.cursor='hand'
-        this.dom = ele;
+    painting(canvas){
+        canvas.style.cursor='hand'
+        this.dom = canvas;
         if(!this.ctx){
-            this.ctx=ele.getContext("2d");  
+            this.ctx=canvas.getContext("2d");  
         } 
         var that = this;
-        ele.onmousedown = function(e){
+        canvas.onmousedown = function(e){
             that.cleanBoard();
-            var x = e.clientX - ele.parentNode.offsetLeft;
-            var y = e.clientY - ele.parentNode.offsetTop;
+            var x = e.clientX - canvas.parentNode.offsetLeft;
+            var y = e.clientY - canvas.parentNode.offsetTop;
             that.ctx.beginPath();
             that.ctx.moveTo(x,y);
             that.ctx.strokeStyle = that.color;
             that.ctx.lineWidth = that.penWidth;
             document.onmousemove = function(e){
                 var e=e||event;
-                var x1= e.clientX- ele.parentNode.offsetLeft;
-                var y1= e.clientY- ele.parentNode.offsetTop;
+                var x1= e.clientX- canvas.parentNode.offsetLeft;
+                var y1= e.clientY- canvas.parentNode.offsetTop;
                 that.ctx.lineTo(x1,y1);
                 that.ctx.stroke();
             }
@@ -34,9 +35,9 @@ class DrawingBoard{
         }
     }
     //清除画布
-    cleanBoard(ele){
-        if(ele){
-            ele.height=ele.height;  
+    cleanBoard(canvas){
+        if(canvas){
+            canvas.height=canvas.height;  
         }
     }
  
@@ -56,5 +57,19 @@ class DrawingBoard{
         //执行下载
         a.click();
     }
-
+    //鼠标图
+    chooseCursor(canvas,goods){
+        switch(goods){
+            case 'pen':
+                canvas.parentNode.classList.add("penStyle");
+                canvas.parentNode.classList.remove("rubberStyle");
+                break;
+            case 'rubber':
+                canvas.parentNode.classList.add("rubberStyle");
+                canvas.parentNode.classList.remove("penStyle");
+                break;
+            default: 
+                break;
+        }
+    }
 }
